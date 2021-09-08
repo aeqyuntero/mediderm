94c316a1cf569d26172f520bf7478e9e0849867a
Esteban Quintero <aeqyuntero@gmail.com>
2021-09-07 23:55:28 -0500

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { SedesComponent } from './pages/sedes/sedes.component';
+import { MedicosComponent } from './pages/medicos/medicos.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'registro', component: RegistroComponent},
+  {path: 'contacto', component: ContactoComponent},
+  {path: 'sedes', component: SedesComponent },
+  {path: 'servicios', component: ServiciosComponent },
+  {path: 'medicos', component: MedicosComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'inicio'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './pages/login/login.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { SedesComponent } from './pages/sedes/sedes.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { MedicosComponent } from './pages/medicos/medicos.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    InicioComponent,
+    RegistroComponent,
+    SedesComponent,
+    ContactoComponent,
+    MedicosComponent,
+    ServiciosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
