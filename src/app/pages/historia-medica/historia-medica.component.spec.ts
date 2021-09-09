0f810aeb7ca7c91f7ea0f3f4dc694441b788eb5e
Cesar Alonso Llano Lagos <43044721+cesarAlonsolll@users.noreply.github.com>
2021-09-09 15:07:51 -0500

    patalla arreglada

---
+++ src/app/pages/historia-medica/historia-medica.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HistoriaMedicaComponent } from './historia-medica.component';
+
+describe('HistoriaMedicaComponent', () => {
+  let component: HistoriaMedicaComponent;
+  let fixture: ComponentFixture<HistoriaMedicaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HistoriaMedicaComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HistoriaMedicaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
