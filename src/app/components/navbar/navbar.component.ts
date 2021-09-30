f59e5dace13e21bfd05cd6406f6fb4824d1675a1
Your NameEsteban <estebanlen@hotmail.com>
2021-09-30 01:03:56 -0500

    Medico terminado

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MedicosComponent } from 'src/app/pages/medicos/medicos.component';
+import { ConexionBDService } from 'src/app/services/conexion-bd.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: ConexionBDService) { }
 
+  medicos: any[] = [];
   ngOnInit(): void {
-  }
+    this.db.getList("/Medicos").subscribe(data => {
+      this.medicos = data ;
+    });
+}
   
   funcion(): void {
     let x = document.getElementById("iniciado");
