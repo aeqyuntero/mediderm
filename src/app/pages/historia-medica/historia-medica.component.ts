56c39063cda67de3e282f49ff3ab7baf071a23b4
Esteban Quintero <aeqyuntero@gmail.com>
2021-09-30 13:40:47 -0500

    crud usuario listo

--- src/app/pages/historia-medica/historia-medica.component.ts
+++ src/app/pages/historia-medica/historia-medica.component.ts
@@ -1,33 +1,75 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../dataservices/data.service';
 
 @Component({
   selector: 'app-historia-medica',
   templateUrl: './historia-medica.component.html',
-  styleUrls: ['./historia-medica.component.css']
+  styleUrls: ['./historia-medica.component.css'],
 })
 export class HistoriaMedicaComponent implements OnInit {
-
-  constructor() { }
+  constructor(private data: DataService) {}
 
   ngOnInit(): void {
+    this.obtenerUsuario();
   }
 
-  funcionDivInfo():void{
-    let div = document.getElementById("info");
-    let div2 = document.getElementById("historia");
-    if(div != null && div2 != null){
-      div.style.display = "none";
-      div2.style.display = "block";
+  funcionDivInfo(): void {
+    let div = document.getElementById('info');
+    let div2 = document.getElementById('historia');
+    if (div != null && div2 != null) {
+      div.style.display = 'none';
+      div2.style.display = 'block';
     }
   }
 
-  funcionDivHistoria():void{
-    let div = document.getElementById("info");
-    let div2 = document.getElementById("historia");
-    if(div != null && div2 != null){
-      div.style.display = "block";
-      div2.style.display = "none";
+  funcionDivHistoria(): void {
+    let div = document.getElementById('info');
+    let div2 = document.getElementById('historia');
+    if (div != null && div2 != null) {
+      div.style.display = 'block';
+      div2.style.display = 'none';
     }
   }
 
+  nombre = '';
+  apellido = '';
+  correo = '';
+  fecha = '';
+  telefono = '';
+  contrasena = '';
+  sexo = '';
+  nomUsuario = '';
+  direccion = '';
+
+  esMasculino = false;
+  loading = true;
+
+  obtenerUsuario() {
+    const id = localStorage.getItem('token');
+    console.log(id);
+
+    if (id) {
+      this.data.obtenerUsuario(id).subscribe((resp: any) => {
+        this.nombre = resp.nombre;
+        this.apellido = resp.apellido;
+        this.correo = resp.correo;
+        this.fecha = resp.fecha;
+        this.telefono = resp.telefono;
+        this.contrasena = resp.contrasena;
+        this.sexo = resp.sexo;
+        this.nomUsuario = resp.nomUsuario;
+        this.direccion = resp.direccion;
+
+        console.log(this.nombre);
+
+        if (this.sexo == 'M') {
+          this.esMasculino = true;
+        } else {
+          this.esMasculino = false;
+        }
+
+        this.loading = false;
+      });
+    }
+  }
 }
